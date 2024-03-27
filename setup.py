09c515fba76b12fbfe7f18240896a4b40f683127
Jaideep Sundaram <jai.python3@gmail.com>
2024-03-27 14:47:53 -0400

    Bump version: 0.4.0 → 0.5.0

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/jai-python3/validation-component-bootstrap-utils',
-    version='0.4.0',
+    version='0.5.0',
     zip_safe=False,
 )
